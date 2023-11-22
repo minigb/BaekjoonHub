713ef0d1856bdecca6cee8b8f21fb5c0b437434d
Minhee Lee <minigimbob@gmail.com>
2023-11-22 23:59:39 +0900

    Change commit message

--- scripts/baekjoon/parsing.js
+++ scripts/baekjoon/parsing.js
@@ -46,7 +46,7 @@ function makeDetailMessageAndReadme(data) {
     code, language, memory, runtime } = data;
   const score = parseNumberFromString(result);
   const directory = `Baekjoon-Online-Judge/${level.replace(/ .*/, '')}/${problemId}. ${convertSingleCharToDoubleChar(title)}`;
-  const message = `[${level}] Title: ${title}, Time: ${runtime} ms, Memory: ${memory} KB`
+  const message = `Upload ${problemId}.`
     + ((isNaN(score)) ? ' ' : `, Score: ${score} point `) // 서브 태스크가 있는 문제로, 점수가 있는 경우 점수까지 커밋 메시지에 표기
     + `-BaekjoonHub`;
   const category = problem_tags.join(', ');
